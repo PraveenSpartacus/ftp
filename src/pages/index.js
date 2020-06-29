1b04b42e04922edcaf68dd16cfc970c32a652f11
Francis Spartacus <francis@Franciss-MacBook-Air.local>
2020-06-30 04:09:41 +0530

    fifth

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,6 +25,7 @@ export class index extends Component {
     render() {
         return (
             <div className="page">
+                <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet"></link>
                 <NavBar/>
                 <HomeContent/>
                 <Featured/>
--- src/pages/netflix.js
+++ src/pages/netflix.js
@@ -14,6 +14,7 @@ import hbo from '../images/hbo.svg'
 const netflix = () =>{
     return(
         <div className={`page ${style.page}`}>
+            <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet"></link>
             <NavBar/>
             <div className={style.pageContent}>
                 <img className={style.img} src={netflixImg}></img>
