dab8a2f98c3c4696f1753ca3fc8fa4d1397739b4
Francis Spartacus <francis@Franciss-MacBook-Air.local>
2020-06-30 03:14:29 +0530

    third

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,8 +7,8 @@ import AniLink from "gatsby-plugin-transition-link/AniLink"
 function Card(props) {
     return (
         
-        
-           <AniLink swipe direction="left" duration={0.8} entryOffset={200} className={style.cardContainer} to={'/'+props.card.title}>
+        // to={'/'+props.card.title}
+           <AniLink swipe direction="left" duration={0.8} entryOffset={200} className={style.cardContainer} to={'/'}>
                 <img src={require(`../images/${props.card.title}.svg`)} alt="img"></img>
                 <p className={style.days}>
                     {props.card.days} days
